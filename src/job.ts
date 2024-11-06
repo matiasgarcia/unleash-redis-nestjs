12fe7d03f2d8cd6ac86d528644408bf1f9cfd6c2
Matias Garcia <matias.garcia@tiendanube.com>
2024-11-06 13:08:21 -0300

    reduce runtime for job

--- src/job.ts
+++ src/job.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.init();
   console.log('Simulate some work');
-  await sleep(1000);
+  await sleep(200);
   console.log('Start shutdown');
   if (!process.env.CLOSE_UNLEASH_AND_REDIS) {
     console.log('wont close unleash nor redis, therefore process will hang');
